36eabb64a84c66568f3288b413dcd875bced56c1
imadhrit <maharajrabel@gmail.com>
2021-09-06 23:34:45 +0530

    run Readme

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
+#/usr/bin/env sh
 
 # check every file
 python maze.py
 python reinforce.py
+
+echo -e "Running Game maybe give incompatible indexer error because of same value of actions []"
 python update.py 
 
 
